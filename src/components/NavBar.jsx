73241fd092e3e3f025e40753b924853101906207
Ray Besselink <20246836@s.ubaguio.edu>
2026-03-22 13:08:03 +0800

    Implemented a Form validation

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,18 +3,23 @@ import './NavBar.css';
 
 const NavBar = () => {
     return (
-        <nav>
-      <ul>
-        <li>
-          <Link to="/home">Home</Link>
-        </li>
-        <li>
-          <Link to="/register">Register</Link>
-        </li>
-        
-      </ul>
+        <nav style={styles.nav}>
+      <Link style={styles.link} to="/home">Home</Link>
+      <Link style={styles.link} to="/register">Register</Link>
     </nav>
     );
-
 }
+const styles = {
+  nav: {
+    padding: "15px",
+    background: "#333",
+    display: "flex",
+    gap: "20px"
+  },
+  link: {
+    color: "white",
+    textDecoration: "none",
+    fontWeight: "bold"
+  }
+};
 export default NavBar;
