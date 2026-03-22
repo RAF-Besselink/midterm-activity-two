e21482c1e0fc95aae8dd6562c1a3593c5ef2c98c
Ray Besselink <20246836@s.ubaguio.edu>
2026-03-22 19:39:15 +0800

    created a student details page

--- src/components/StudentList.jsx
+++ src/components/StudentList.jsx
@@ -4,7 +4,11 @@ export default function StudentList({ students }) {
   return (
     <div style={{ padding: "20px" }}>
       <h2>Registered Students</h2>
+
       {students.length === 0 && <p>No registered students yet.</p>}
+
+      {students.length === 0 && <p>No students yet.</p>}
+
       <ul>
         {students.map((student, index) => (
           <li key={index} style={{ marginBottom: "10px" }}>
